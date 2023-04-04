a92a928b2e895c850c274b1cbcf1cdf7129e903d
DenisOSVR <denis.masek@student.gyarab.cz>
2023-04-04 11:19:29 +0200

    idfk

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Movie
+from .models import Director
 
 def homepage(request):
     context = {
         'movies': Movie.objects.all(),
     }
     return render(request, 'main.html', context)
+
+def directors(request):
+    context = {
+        'title': 'Nej režiséři',
+        'directors': Director.objects.all()
+    }
+    return render(request, 'directors.html', context)
--- gamdb/gamdb/urls.py
+++ gamdb/gamdb/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from films.views import homepage
+from films.views import directors
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path('reziseri', directors),
 ]
